db5c7ea678300cd16faa915bda3204b499353356
Juan Pablo <juanpablom6789@gmail.com>
2024-06-08 12:52:19 -0300

    Changes to be committed:     modified:   src/Modulos/Types.fs

--- src/Modulos/Types.fs
+++ src/Modulos/Types.fs
@@ -3,6 +3,16 @@
 module Types = 
 
     type Player
-        | PosX of 
+        | PosX of int
+        | PosY of int
+        | Height of int
+        | Width of int
+        | Water of bool
 
-    
+    type Turtle
+        | PosX of int
+        | PosY of int
+        | Height of int
+        | Width of int
+        | Speed of int
+        | Timer of int
