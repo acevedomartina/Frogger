da9d5315d772351819b52a4ac4095f18c83ae113
Martina Acevedo <martina.acevedo@ib.edu.ar>
2024-06-08 12:53:56 -0300

    Agrego Obstacle, Final_row

--- src/Modulos/Types.fs
+++ src/Modulos/Types.fs
@@ -5,4 +5,15 @@ module Types =
     type Player
         | PosX of 
 
+    type Obstacle
+        | PosX of int
+        | PosY of int
+        | Height of int
+        | Width of int
+        | Speed of int  
+
+    type Final_row
+        | PosX of int
+        | Width of int
+        | Ocupation of bool
     
