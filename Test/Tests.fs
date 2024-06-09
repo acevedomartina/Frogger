14f06bfc7dbdcefe99c5b49e7f30b2bdf0fd66b1
Martina Acevedo <martina.acevedo@ib.edu.ar>
2024-06-09 12:51:15 -0300

    Test

--- Test/Tests.fs
+++ Test/Tests.fs
@@ -3,7 +3,13 @@ module Tests
 open Xunit
 open Frogger.Modulos.Functions
 
+
 [<Fact>]
-let ``My test`` () =
-    Assert.True(true)
-cd
+let ``moveDown Two should return One`` () =
+    let result = Functions.moveDown Rows.Two
+    Assert.Equal(Rows.One, result)
+
+[<Fact>]
+let ``moveUp Two should return Three`` () =
+    let result = Functions.moveUp Rows.Two
+    Assert.Equal(Rows.Three, result)
