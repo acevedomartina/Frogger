2d46cd81154dfb026193ea806afb4ad516bc6ff8
Juan Pablo <juanpablom6789@gmail.com>
2024-06-09 11:54:01 -0300

    Actualizados funcion checkPlayerLost

--- src/Modulos/Functions.fsx
+++ src/Modulos/Functions.fsx
@@ -1,123 +1,121 @@
 ﻿namespace Frogger.Modulos
 
-let WIDTH = 800
-let HEIGHT = 600
-let WIDTH_PLAYABLE_LEFT = 100 
-let WIDTH_PLAYABLE_RIGHT = 700
+open Frogger.Modulos.Types
 
-// Función que mueve de una fila hacia la fila de arriba
-let moveUp (posY : Rows) = 
-    match posY with
-    | One -> Two
-    | Two -> Three
-    | Three -> Four
-    | Four -> Five
-    | Five -> Six
-    | Six -> Seven
-    | Seven -> Six
-    | Eight -> Nine
-    | Nine -> Ten
-    | Ten -> Eleven
-    | Eleven -> Twelve
-    | Twelve -> Thirteen 
-    | _ -> raise (System.Exception("Error no deberia pasar"))
 
-// Función que mueve de una fila hacia la fila de abajo
-let moveDown (posY : Rows) = 
-    match posY with
-    | One -> One
-    | Two -> One
-    | Three -> Two
-    | Four -> Three
-    | Five -> Four
-    | Six -> Five
-    | Seven -> Six
-    | Eight -> Seven
-    | Nine -> Eight
-    | Ten -> Nine
-    | Eleven -> Ten
-    | Twelve -> Eleven
-    | _ -> raise (System.Exception("Error no deberia pasar"))
+module Functions =
 
-// Función que mueve al jugador en la dirección indicada
-let movePlayer (player : Player) (dir: Direction)  = 
-    match dir with
-    | Up -> let newPosY = moveUp player.PosY
-            {player with PosY = newPosY}
-    | Down -> let newPosY = moveDown player.PosY
-              {player with PosY = newPosY}
-    | Left -> if player.PosX - 50 < WIDTH_PLAYABLE_LEFT then player else {player with PosX = player.PosX - 50}    
-    | Right -> if player.PosX + 50 > WIDTH_PLAYABLE_RIGHT then player else {player with PosX = player.PosX + 50}
+    let WIDTH = 800
+    let HEIGHT = 600
+    let WIDTH_PLAYABLE_LEFT = 100 
+    let WIDTH_PLAYABLE_RIGHT = 700
 
-// Chequeamos que haya colisión entre el jugador y el obstáculo
-let checkCollision (player : Player) (obstacle : Obstacle) =
-    (player.PosX + player.Width/2 > obstacle.x_left || player.PosX - player.Width/2 < obstacle.x_right)
+    // Función que mueve de una fila hacia la fila de arriba
+    let moveUp (posY : Rows) = 
+        match posY with
+        | One -> Two
+        | Two -> Three
+        | Three -> Four
+        | Four -> Five
+        | Five -> Six
+        | Six -> Seven
+        | Seven -> Six
+        | Eight -> Nine
+        | Nine -> Ten
+        | Ten -> Eleven
+        | Eleven -> Twelve
+        | Twelve -> Thirteen 
+        | _ -> raise (System.Exception("Error no deberia pasar"))
 
-// Chequeo si el jugador no está arriba de un tronco o una tortuga
-let checkNotUpLogTurtle (player : Player) (obstacle : Obstacle) = 
-    // Si la tortuga no está sumergida veo que pasa
-    if not obstacle.Underwater then
-        // Condición para que esté totalmente arriba del obsáculo
-        if obstacle.x_left < player.PosX - player.Width / 2 && obstacle.x_right > player.PosX + player.Width / 2 then
-            false
-        // Si se teletransporta al otro lado el obstaculo pasa esto
-        else if obstacle.x_left > obstacle.x_right then
-            // Si el extremo derecho del jugador está a la derecha del obstáculo me ahogo entonces devuelve true
-            // En caso contrario no me ahogo y devuelve false
-            (obstacle.x_right < player.PosX + player.Width / 2)
-        // Si el extremo izquierdo del jugador esta a la derecha del extremo izquierdo del obstáculo no me ahogo devuelve false
-        // En caso contrario devuelve true
-        else if obstacle.x_left < player.PosX - player.Width / 2 then
-            false
+    // Función que mueve de una fila hacia la fila de abajo
+    let moveDown (posY : Rows) = 
+        match posY with
+        | One -> One
+        | Two -> One
+        | Three -> Two
+        | Four -> Three
+        | Five -> Four
+        | Six -> Five
+        | Seven -> Six
+        | Eight -> Seven
+        | Nine -> Eight
+        | Ten -> Nine
+        | Eleven -> Ten
+        | Twelve -> Eleven
+        | _ -> raise (System.Exception("Error no deberia pasar"))
+
+    // Función que mueve al jugador en la dirección indicada
+    let movePlayer (player : Player) (dir: Direction)  = 
+        match dir with
+        | Up -> let newPosY = moveUp player.PosY
+                {player with PosY = newPosY}
+        | Down -> let newPosY = moveDown player.PosY
+                {player with PosY = newPosY}
+        | Left -> if player.PosX - 50 < WIDTH_PLAYABLE_LEFT then player else {player with PosX = player.PosX - 50}    
+        | Right -> if player.PosX + 50 > WIDTH_PLAYABLE_RIGHT then player else {player with PosX = player.PosX + 50}
+
+    // Chequeamos que haya colisión entre el jugador y el obstáculo
+    let checkCollision (player : Player) (obstacle : Obstacle) =
+        (player.PosX + player.Width/2 > obstacle.x_left || player.PosX - player.Width/2 < obstacle.x_right)
+
+    // Chequeo si el jugador no está arriba de un tronco o una tortuga
+    let checkNotUpLogTurtle (player : Player) (obstacle : Obstacle) = 
+        // Si la tortuga no está sumergida veo que pasa
+        if not obstacle.Underwater then
+            // Condición para que esté totalmente arriba del obsáculo
+            if obstacle.x_left < player.PosX - player.Width / 2 && obstacle.x_right > player.PosX + player.Width / 2 then
+                false
+            // Si se teletransporta al otro lado el obstaculo pasa esto
+            else if obstacle.x_left > obstacle.x_right then
+                // Si el extremo derecho del jugador está a la derecha del obstáculo me ahogo entonces devuelve true
+                // En caso contrario no me ahogo y devuelve false
+                (obstacle.x_right < player.PosX + player.Width / 2)
+            // Si el extremo izquierdo del jugador esta a la derecha del extremo izquierdo del obstáculo no me ahogo devuelve false
+            // En caso contrario devuelve true
+            else if obstacle.x_left < player.PosX - player.Width / 2 then
+                false
+            else
+                true
+        
         else
             true
-    
-    else
-        true
 
-// Función que mueve un obstáculo y establece las condiciones periódicas de contorno
-let moveObstacle (obstacle: Obstacle) =
-    let x_left_new : int = (obstacle.x_left + obstacle.Speed) % WIDTH 
-    let x_right_new : int = (obstacle.x_right + obstacle.Speed) % WIDTH
-    {obstacle with x_left = x_left_new; x_right = x_right_new}
+    // Función que mueve un obstáculo y establece las condiciones periódicas de contorno
+    let moveObstacle (obstacle: Obstacle) =
+        let x_left_new : int = (obstacle.x_left + obstacle.Speed) % WIDTH 
+        let x_right_new : int = (obstacle.x_right + obstacle.Speed) % WIDTH
+        {obstacle with x_left = x_left_new; x_right = x_right_new}
 
-// Función para cambiar el estado de la tortuga i-ésima cada 10 segundos
-let state_tortuga (idx : int) (tiempo: int) (obstacles: Obstacle list) =
-    if tiempo % 10 = 0 then
-        obstacles |> List.mapi (fun i x -> if i = idx then {x with Underwater = not x.Underwater} else x)
-    else
-        obstacles
+    // Función para cambiar el estado de la tortuga i-ésima cada 10 segundos
+    let state_tortuga (idx : int) (tiempo: int) (obstacles: Obstacle list) =
+        if tiempo % 10 = 0 then
+            obstacles |> List.mapi (fun i x -> if i = idx then {x with Underwater = not x.Underwater} else x)
+        else
+            obstacles
 
-// Función para actualizar el fondo del juego, los obstáculos y el estado de las tortugas
-let updateFondo (fondo : Fondo) = 
-    // Cambiamos todos los obstáculos moviendolos
-    let movedObstacles = Map.map (fun _ lst -> lst |> List.map moveObstacle) fondo.Obstacles
-    
-    // Cambiamos el estado de las tortugas en las filas 8 y 11 cada 10 segundos
-    let idx = 1 // Como hay dos y tres tortugas en las filas 8 y 11 respectivamente cambiamos el estado de la segunda tortuga en cada fila
-    let updatedObstcles = movedObstacles |> Map.map (fun key lst -> lst |> List.mapi (fun idx obs ->  if key = Rows.Eight || key = Rows.Eleven then
-                                                                                                        state_tortuga idx fondo.Time lst |> List.item idx
-                                                                                                      else
-                                                                                                        obs))
-    // Return the updated fondo
-    {fondo with Obstacles = updatedObstcles}
-
-let updateGameState (game : GameState) (dir : Option<Direction>) = 
-        match dir with
-        | Some d -> let newFondo = updateFondo game.Fondo
-                    let newPlayer = movePlayer game.Player d
-                    {game with Player = newPlayer; Fondo = newFondo}
-                
-        | None -> let newFondo = updateFondo game.Fondo
-                  {game with Fondo = newFondo}
+    // Función para actualizar el fondo del juego, los obstáculos y el estado de las tortugas
+    let updateFondo (fondo : Fondo) = 
+        // Cambiamos todos los obstáculos moviendolos
+        let movedObstacles = Map.map (fun _ lst -> lst |> List.map moveObstacle) fondo.Obstacles
         
-// Chequear si se acabó el tiempo
-let CheckTime (game: GameState) = 
-    if game.Fondo.Time = 0 then
-        {game with Lifes = GameOver}
-    else
-        game
-    
+        // Cambiamos el estado de las tortugas en las filas 8 y 11 cada 10 segundos
+        let idx = 1 // Como hay dos y tres tortugas en las filas 8 y 11 respectivamente cambiamos el estado de la segunda tortuga en cada fila
+        let updatedObstcles = movedObstacles |> Map.map (fun key lst -> lst |> List.mapi (fun idx obs ->  if key = Rows.Eight || key = Rows.Eleven then
+                                                                                                            state_tortuga idx fondo.Time lst |> List.item idx
+                                                                                                          else
+                                                                                                            obs))
+        // Return the updated fondo
+        {fondo with Obstacles = updatedObstcles}
+
+    let updateGameState (game : GameState) (dir : Option<Direction>) = 
+            match dir with
+            | Some d -> let newFondo = updateFondo game.Fondo
+                        let newPlayer = movePlayer game.Player d
+                        {game with Player = newPlayer; Fondo = newFondo}
+                    
+            | None -> let newFondo = updateFondo game.Fondo
+                    {game with Fondo = newFondo}
+            
 //////////////////////////////////////////////////////////////////////////////7
 
     // Chequear si se acabó el tiempo
@@ -147,7 +145,7 @@ let CheckTime (game: GameState) =
         let anyGoalSpaceChanged = List.exists (fun gs -> gs.Ocupation) updatedGoalSpaces
 
         if anyGoalSpaceChanged then
-            { game with Final_row = updatedGoalSpaces }
+            { game with Final_row = updatedGoalSpaces; Score = game.Score + 50}
         else
             updateLives game
         
@@ -174,34 +172,38 @@ let CheckTime (game: GameState) =
         | GameOver -> Error "Game Over"
         | _ -> Ok game
 
-    let checkPlayerLose (game : GameState) =
+    let checkPlayerLose (game : GameState) : Result<GameState, string>=
         let player = game.Player
         let PosY = player.PosY
         match PosY with
-        | One | Seven -> game
+        | One | Seven -> Ok game
         | Two | Three | Four | Five | Six ->    let obstacles = Map.find PosY game.Fondo.Obstacles
                                                 let collision = List.exists (checkCollision player) obstacles
                                                 if collision then
-                                                    let newGame = checkLoseGame (updateLives game)
-                                                    if newGame.IsOk then
-                                                        let newPlayer = {player with PosX = WIDTH/2; PosY = Rows.One}
-                                                        let newScore = game.Score + 10 
-                                                        Ok {newGame with Player = newPlayer, Score = newScore}
-                                                    else
-                                                        newGame
+                                                    let newGame = updateLives game
+                                                    match newGame.Lifes with
+                                                    | GameOver -> Error "Game Over"
+                                                    | _ -> let newPlayer = {player with PosX = WIDTH/2; PosY = Rows.One}
+                                                        let newScore = game.Score + 10
+                                                        Ok {newGame with Player = newPlayer; Score = newScore}
                                                 else
                                                     Ok game
+
         | Eight | Nine | Ten | Eleven | Twelve ->   let obstacles = Map.find PosY game.Fondo.Obstacles
                                                     let drown = List.exists (checkNotUpLogTurtle player) obstacles
                                                     if drown then
-                                                        let newGame = checkLoseGame (updateLives game)
-                                                        if newGame.IsOk then
-                                                            let newPlayer = {player with PosX = WIDTH/2; PosY = Rows.One}
-                                                            let newScore = game.Score + 10
-                                                            Ok {newGame with Player = newPlayer, Score = newScore}
-                                                        else
-                                                            newGame
+                                                        let newGame = updateLives game
+                                                        match newGame.Lifes with
+                                                            | GameOver -> Error "Game Over"
+                                                            | _ -> let newPlayer = {player with PosX = WIDTH/2; PosY = Rows.One}
+                                                                let newScore = game.Score + 10
+                                                                Ok {newGame with Player = newPlayer; Score = newScore}
                                                     else
                                                         Ok game
-
-        | Thirteen -> //Chequear si ganó o perdío una vida
+        | Thirteen -> let newGame = CheckGoal game
+                    match newGame.Lifes with
+                        | GameOver -> Error "Game Over"
+                        | _ -> let newGame = CheckWin newGame
+                            match newGame.Lifes with
+                            | GameOver -> Error "Game Over"
+                            | _ -> Ok newGame
