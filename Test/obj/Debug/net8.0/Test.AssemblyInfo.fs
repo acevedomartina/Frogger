c47feec3b44722ee046b97d1bf6a7bf3ec1d2d44
Martina Acevedo <martina.acevedo@ib.edu.ar>
2024-06-09 15:54:56 -0300

    Prueba

--- Test/obj/Debug/net8.0/Test.AssemblyInfo.fs
+++ Test/obj/Debug/net8.0/Test.AssemblyInfo.fs
@@ -10,7 +10,7 @@ open System.Reflection
 [<assembly: System.Reflection.AssemblyCompanyAttribute("Test")>]
 [<assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")>]
 [<assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")>]
-[<assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+14f06bfc7dbdcefe99c5b49e7f30b2bdf0fd66b1")>]
+[<assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7eb2dc2456262883e887d21870c022c6dd20f301")>]
 [<assembly: System.Reflection.AssemblyProductAttribute("Test")>]
 [<assembly: System.Reflection.AssemblyTitleAttribute("Test")>]
 [<assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")>]
--- src/obj/Debug/net8.0/Frogger.AssemblyInfo.fs
+++ src/obj/Debug/net8.0/Frogger.AssemblyInfo.fs
@@ -10,7 +10,7 @@ open System.Reflection
 [<assembly: System.Reflection.AssemblyCompanyAttribute("Frogger")>]
 [<assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")>]
 [<assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")>]
-[<assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b0ee99b10511d7fcc5a4cdbba821ae6376c1ccb8")>]
+[<assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7eb2dc2456262883e887d21870c022c6dd20f301")>]
 [<assembly: System.Reflection.AssemblyProductAttribute("Frogger")>]
 [<assembly: System.Reflection.AssemblyTitleAttribute("Frogger")>]
 [<assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")>]
